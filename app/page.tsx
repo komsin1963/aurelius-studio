5e3169623d615fc27e09717f348f94574b18cbb0
komsin1963 <Kom_art@yahoo.com>
2026-01-15 14:24:35 +0700

    redeploy please

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,6 @@ export default function Home() {
         <Link href="/studio3">ไปที่ Studio 3 (AI Generator)</Link> | 
         <Link href="/studio4">ไปที่ Studio 4</Link>
       </nav>
-    </div>
+    </div> 
   );
 }
